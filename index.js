0a4c9dec1065690afea19e0b1e2e4688826a90e8
Evandro Eisinger <evandroeisinger@gmail.com>
2014-07-31 11:33:46 -0300

    Fix the CSS prepend path

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 #! /usr/bin/env node
 
-
 /*
   * node-compressor: a complete solution to compress static files with node
   * http://github.com/EvandroLG/node-compressor
@@ -9,21 +8,23 @@
   * License: MIT
 */
 
-
 'use strict';
-
 
 var fs = require('fs');
 var path = require('path');
 var Params = require('./lib/params');
 var Compressor = require('./lib/compressor');
 
-
 var main = function(file) {
+
+  var root = Params.root || path.dirname();
+  
   fs.readFile(file, 'utf8', function(err, data) {
-    var root = Params.root || path.dirname();
+
     new Compressor(err, data, root, file);
+
   });
+
 };
 
 var files = Params.read().files;
